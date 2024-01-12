dda8ded808f67e91fbe8b34e792ef18b95399f7f
hk <ihkkeep@gmail.com>
2024-01-12 22:15:26 +0800

    update custom.sh and build.yml

--- custom.sh
+++ custom.sh
@@ -1,13 +1,52 @@
-#####################################################################
-# 修改默认参数
-#####################################################################
+#!/bin/bash
 
-lan_ip="192.168.31"                             # LAN 地址 别写后面的 .1
-default_path="./user/shared"                    # 默认配置路径
-config="./configs/templates/RM2100.config"      # 默认配置文件
+user_name="root"                                    # 用户名
+user_password="root"                                # 登录密码
+lan_ip="192.168.31"                                 # LAN 地址 别写后面的 .1
+wlan_2g_ssid="int_2.4G"                             # 2G 无线名称
+wlan_5g_ssid="int_5G"                               # 5G 无线名称
+wlan_guest_2g_ssid="int_pub_2.4G"                   # 2G 访客无线名称
+wlan_guest_5g_ssid="int_pub_5G"                     # 5G 访客无线名称
+wlan_2g_psk="qwerasdf"                              # 2G WIFI密码最少8位 空白为不设置
+wlan_5g_psk="qwerasdf"                              # 5G WIFI密码最少8位 空白为不设置
+version_time=$(date +%Y%m%d)                        # 更新时版本号时间: 20210101
+default_file="./user/shared/defaults.h"             # 默认配置文件
+
+echo "修改用户名"
+sed -i 's/SYS_USER_ROOT		"admin"/SYS_USER_ROOT		"'$user_name'"/g' $default_file
+
+echo "修改登陆密码"
+sed -i 's/DEF_ROOT_PASSWORD	"admin"/DEF_ROOT_PASSWORD	"'$user_password'"/g' $default_file
 
 echo "修改 LAN IP 地址"
-sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
+sed -i "s/192.168.2/$lan_ip/g" $default_file
+
+echo "修改 2G 无线名称"
+sed -i 's/DEF_WLAN_2G_SSID	BOARD_PID "_%s"/DEF_WLAN_2G_SSID	"'$wlan_2g_ssid'"/g' $default_file
+
+echo "修改 5G 无线名称"
+sed -i 's/DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"/DEF_WLAN_5G_SSID	"'$wlan_5g_ssid'"/g' $default_file
+
+echo "修改 2G 访客无线名称"
+sed -i 's/DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"/DEF_WLAN_2G_GSSID	"'$wlan_guest_2g_ssid'"/g' $default_file
+
+echo "修改 5G 访客无线名称"
+sed -i 's/DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"/DEF_WLAN_5G_GSSID	"'$wlan_guest_5g_ssid'"/g' $default_file
+
+echo "修改 2.4GHz WIFI 密码"
+sed -i 's/DEF_WLAN_2G_PSK		"1234567890"/DEF_WLAN_2G_PSK		"'$wlan_2g_psk'"/g' $default_file
+
+echo "修改 5GHz WIFI 密码"
+sed -i 's/DEF_WLAN_5G_PSK		"1234567890"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
+
+echo "更新版本号时间"
+sed -i "s/FIRMWARE_BUILDS_REV=.*/FIRMWARE_BUILDS_REV=$version_time/g" ./versions.inc
+
+
+
+#####################################################################
+
+default_path="./user/shared"                    # 默认配置路径
 
 #echo "设置为PPPOE模式并写入账号和密码"
 #sed -i 's/{ "wan_proto", "dhcp" }/{ "wan_proto", "pppoe" }/g' $default_path/defaults.c
@@ -21,16 +60,4 @@ sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
 #sed -i 's/{ "rt_mode_x", "0" }/{ "rt_mode_x", "4" }/g' $default_path/defaults.c
 
 #echo "打开5G无线桥接"
-sed -i 's/{ "wl_mode_x", "0" }/{ "wl_mode_x", "4" }/g' $default_path/defaults.c
-
-#CPU超频
-#echo "CONFIG_FIRMWARE_INCLUDE_OC=n" >> $config #CPU超频开关
-#echo 'CONFIG_FIRMWARE_MT7621_OC="0x312"' >> $config #此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
-
-#echo "集成科学插件"
-#grep "CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=n" $config
-#if [ $? -eq 0 ]; then
-#  for MOD in SHADOWSOCKS XRAY TROJAN SSOBFS IPSET EAP_PEAP OPENSSH OPENSSL_EC OPENSSL_EXE CURL; do
-#    sed -i "s/CONFIG_FIRMWARE_INCLUDE_${MOD}=n/CONFIG_FIRMWARE_INCLUDE_${MOD}=y/g" $config
-#  done
-#fi
+#sed -i 's/{ "wl_mode_x", "0" }/{ "wl_mode_x", "4" }/g' $default_path/defaults.c
